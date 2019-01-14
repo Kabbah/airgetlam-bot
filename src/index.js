aa4c315038f7fe91382bd705c5cd57bafb06522d
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2019-01-14 00:21:31 -0200

    Adicionado comando com parse de número para remover mensagems.

--- src/index.js
+++ src/index.js
@@ -59,6 +59,21 @@ client.on("message", message => {
 
         message.channel.send(avatarList);
     }
+    else if (command === "prune") {
+        const amount = parseInt(args[0]);
+    
+        if (isNaN(amount)) {
+            return message.reply("that doesn't seem to be a valid number.");
+        }
+        else if (amount < 2 || amount > 100) {
+            return message.reply("you need to input a number between 2 and 100.");
+        }
+    
+        message.channel.bulkDelete(amount, true).catch(err => {
+            console.error(err);
+            message.channel.send("There was an error trying to prune messages in this channel!");
+        });
+    }
 });
 
 client.login(config.token);
