9fc60f2dbf10bd3637305bdd92a618e1dfe81fac
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2019-01-14 00:00:11 -0200

    Adicionada regex para pegar mais de um espaço

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ client.once("ready", () => {
 client.on("message", message => {
     if (!message.content.startsWith(config.prefix) || message.author.bot) return;
 
-    const args = message.content.slice(config.prefix.length).split(" ");
+    const args = message.content.slice(config.prefix.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
     if (command === "tbdc") {
