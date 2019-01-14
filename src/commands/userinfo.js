63c38c5a0b43d5c7a96091a5adb1c2e68721ea20
Victor Barpp Gomes <bioniclenuparu@gmail.com>
2019-01-14 18:24:19 -0200

    Cooldown de comandos.

--- src/commands/userinfo.js
+++ src/commands/userinfo.js
@@ -1,6 +1,7 @@
 module.exports = {
     name: "userinfo",
     description: "Prints user information.",
+    cooldown: 5,
     execute(message, args) {
         message.channel.send("Your username: " + message.author.username +
                              "\nYour ID: " + message.author.id);
--- src/index.js
+++ src/index.js
@@ -6,11 +6,14 @@ const config = require("./config.json");
 const client = new Discord.Client();
 
 client.commands = new Discord.Collection();
-const commandFiles = fs.readdirSync(config.basedir + "/src/commands").filter(file => file.endsWith(".js"));
+const commandFiles = fs.readdirSync(config.basedir + "/src/commands").filter(
+    file => file.endsWith(".js"));
 for (const file of commandFiles) {
     const command = require("./commands/" + file);
     client.commands.set(command.name, command);
 }
+
+const cooldowns = new Discord.Collection();
 
 client.once("ready", () => {
     console.log("Ready!");
@@ -33,11 +36,34 @@ client.on("message", message => {
         let reply = "you didn't provide any arguments!";
         
         if (command.usage) {
-            reply += "\nThe proper usage would be: `" + config.prefix + command.name + " " + command.usage + "`";
+            reply += "\nThe proper usage would be: " +
+                     "`" + config.prefix + command.name + " " + command.usage + "`";
         }
 
         return message.reply(reply);
     }
+
+    if (!cooldowns.has(command.name)) {
+        cooldowns.set(command.name, new Discord.Collection());
+    }
+    
+    const now = Date.now();
+    const timestamps = cooldowns.get(command.name);
+    const cooldownAmount = (command.cooldown || 3) * 1000;
+    
+    if (timestamps.has(message.author.id)) {
+        const expirationTime = timestamps.get(message.author.id) + cooldownAmount;
+
+        if (now < expirationTime) {
+            const timeLeft = (expirationTime - now) / 1000;
+            return message.reply("please wait " + timeLeft.toFixed(1) + 
+                                 " more second(s) before reusing the `" +
+                                 command.name + "` command.");
+        }
+    }
+
+    timestamps.set(message.author.id, now);
+    setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
     try {
         command.execute(message, args);
